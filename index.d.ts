e42e70cc5f5c264a022427eede167dcbf4856ed0
Fairy-Phy <phy.public@gmail.com>
2024-01-18 17:42:36 +0900

    impl iter

--- index.d.ts
+++ index.d.ts
@@ -122,7 +122,45 @@ interface String {
 	codepoint_at(i: num): num | null;
 }
 
+// S: es2015.iterable.d.ts
+interface SymbolConstructor {
+	/**
+	 * A method that returns the default iterator for an object. Called by the semantics of the
+	 * for-of statement.
+	 */
+	readonly iterator: unique symbol;
+}
+declare var Symbol: SymbolConstructor;
+
+interface IteratorYieldResult<TYield> {
+	done?: false;
+	value: TYield;
+}
+
+interface IteratorReturnResult<TReturn> {
+	done: true;
+	value: TReturn;
+}
+
+type IteratorResult<T, TReturn = any> = IteratorYieldResult<T> | IteratorReturnResult<TReturn>;
+interface Iterator<T, TReturn = any, TNext = undefined> {
+	// NOTE: 'next' is defined using a tuple to ensure we report the correct assignability errors in all places.
+	next(...args: [] | [TNext]): IteratorResult<T, TReturn>;
+	return?(value?: TReturn): IteratorResult<T, TReturn>;
+	throw?(e?: any): IteratorResult<T, TReturn>;
+}
+
+interface Iterable<T> {
+	[Symbol.iterator](): Iterator<T>;
+}
+interface IterableIterator<T> extends Iterator<T> {
+	[Symbol.iterator](): IterableIterator<T>;
+}
+// E: es2015.iterable.d.ts
+
 interface Array<T> {
+	// typescreiptとの互換性専用
+	[Symbol.iterator](): IterableIterator<T>;
 	/**
 	 * 配列の要素数を取得します。
 	 */
@@ -723,6 +761,7 @@ declare interface Async {
 	timeout(delay: num, callback: fn): fn;
 }
 declare var Async: Async;
+
 /**
  * 現在のユーザーID
  */
