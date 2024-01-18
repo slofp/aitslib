9a8d40129756a452d6ec20c6066950670ebd4d67
Fairy-Phy <phy.public@gmail.com>
2024-01-18 10:51:11 +0900

    misskey api doc

--- index.d.ts
+++ index.d.ts
@@ -723,79 +723,309 @@ declare interface Async {
 	timeout(delay: num, callback: fn): fn;
 }
 declare var Async: Async;
+/**
+ * 現在のユーザーID
+ */
 declare const USER_ID: str | null;
+/**
+ * 現在のユーザーの名前
+ */
 declare const USER_NAME: str | null;
+/**
+ * 現在のユーザーの表示名
+ */
 declare const USER_USERNAME: str | null;
 
 interface IEmoji {
+	/**
+	 * 絵文字ID
+	 */
 	id: str;
+	/**
+	 * 絵文字のタグ
+	 */
 	aliases: arr<str>;
+	/**
+	 * 絵文字の名前
+	 */
 	name: str;
+	/**
+	 * 絵文字のカテゴリー
+	 */
 	category: str;
+	/**
+	 * この絵文字がどのhostにあるか
+	 */
 	host: str | null;
+	/**
+	 * 絵文字の画像URL
+	 */
 	url: str;
+	/**
+	 * 絵文字のライセンス
+	 */
 	license: str;
+	/**
+	 * 絵文字がセンシティブかどうか
+	 */
 	isSensitive: bool;
+	/**
+	 * 絵文字がローカルのみかどうか
+	 */
 	localOnly: bool;
+	/**
+	 * 絵文字が使えるロール
+	 *
+	 * (おそらく未実装)
+	 */
 	roleIdsThatCanBeUsedThisEmojiAsReaction: arr;
+	/**
+	 * 絵文字を使用することができないロール
+	 *
+	 * (おそらく未実装)
+	 */
 	roleIdsThatCanNotBeUsedThisEmojiAsReaction: arr;
 }
+/**
+ * ローカルキャッシュのカスタム絵文字
+ */
 declare const CUSTOM_EMOJIS: arr<IEmoji>;
 
+/**
+ * 現在の言語
+ */
 declare const LOCALE: str;
+/**
+ * サーバーのURL
+ *
+ * ここで取得されるのはオリジンのみです。パスまで取得したい場合は`Mk.url()`を使用してください。
+ */
 declare const SERVER_URL: str;
 
 type ModalIconType = 'info' | 'success' | 'warn' | 'error' | 'question';
 declare interface Mk {
+	/**
+	 * ダイアログを表示します。
+	 * @param title - タイトル
+	 * @param text - テキスト
+	 * @param type - アイコン表記
+	 */
 	dialog(title: str, text: str, type?: ModalIconType): null;
+	/**
+	 * 
+	 * @param title - タイトル
+	 * @param text - テキスト
+	 * @param type - アイコン表記
+	 * @returns 「はい」を選択したら`true`、「いいえ」を選択したら`false`になります
+	 */
 	confirm(title: str, text: str, type?: ModalIconType): bool;
+	/**
+	 * 現在のサーバーのapiを送信します。
+	 * 
+	 * サーバーが使用できる各エンドポイントはAPI Consoleまたはapi/endpointsを参照してください。
+	 * @param endpoint - エンドポイント
+	 * @param params - 各エンドポイントが要求するパラメーター
+	 * @param token - apiトークン(必要であれば)。
+	 */
 	api(endpoint: str, params: obj, token?: str): obj | Error;
+	/**
+	 * データをkey-valueでローカルストレージに保存します。
+	 * @param key - キー
+	 * @param value - 値
+	 */
 	save(key: str, value: value): null;
+	/**
+	 * キーに対応したデータをローカルストレージから取得します。
+	 * @param key - キー
+	 */
 	load(key: str): value;
+	/**
+	 * 現在のurlを取得します。
+	 * 
+	 * オリジンのみを取得したい場合は`SERVER_URL`を推奨します。
+	 */
 	url(): str;
+	/**
+	 * テキストをnayizeします。
+	 * @param text - テキスト
+	 */
 	nyaize(text: str): str;
 }
+/**
+ * misskey lib
+ */
 declare var Mk: Mk;
 
 declare interface Plugin {
+	/**
+	 * メタデータで定義したコンフィグの設定データ
+	 *
+	 * 使用するにはメタデータでconfigを定義してください
+	 */
 	readonly config: obj;
-	register_post_form_action(title: str, fn: (form: { text: str, cw: str }, update: (key: 'text' | 'cw', value: str) => void) => void): void;
-	register_note_action(title: str, fn: (user: obj) => void): void;
-	register_user_action(title: str, fn: (note: obj) => void): void;
+	/**
+	 * 投稿フォームにアクションを追加します。
+	 * @param title - アクションメニューに表示する名前
+	 * @param fn - アクション。引数にノートオブジェクトと更新関数を取り、更新関数では指定したキーを値に置き換えます。
+	 */
+	register_post_form_action(
+		title: str,
+		fn: (
+			form: { text: str; cw: str },
+			update: (key: "text" | "cw", value: str) => void
+		) => void
+	): void;
+	/**
+	 * ノートアクションを追加します。
+	 * @param title - アクションメニューに表示する名前
+	 * @param fn - アクション。引数にノートオブジェクトを取ります。
+	 */
+	register_note_action(title: str, fn: (note: obj) => void): void;
+	/**
+	 * ユーザーアクションを追加します。
+	 * @param title - アクションメニューに表示する名前
+	 * @param fn - アクション。引数にユーザーオブジェクトを取ります。
+	 */
+	register_user_action(title: str, fn: (user: obj) => void): void;
+	/**
+	 * ノートをレンダーする際に実行される処理を追加します。
+	 * @param fn - アクション。引数にノートオブジェクトを取り、返り値にはノートオブジェクトを返します。null(void)を返した場合はノートは削除されます。
+	 */
 	register_note_view_interruptor(fn: (note: obj) => obj | void): void;
+	/**
+	 * ノートが投稿される際に実行される処理を追加します。
+	 * @param fn - アクション。引数にノートオブジェクトを取り、返り値にはノートオブジェクトを返します。
+	 */
 	register_note_post_interruptor(fn: (note: obj) => obj): void;
+	/**
+	 * ページの内容をレンダーする際に実行される処理を追加します。
+	 * @param fn - アクション。引数にページオブジェクトを取り、返り値にはページオブジェクトを返します。
+	 */
 	register_page_view_interruptor(fn: (page: obj) => obj): void;
+	/**
+	 * 指定したurlに移動します。
+	 *
+	 * urlは同一オリジンであっても新規タブで開かれます。
+	 * @param url
+	 */
 	open_url(url: str): void;
 }
+/**
+ * プラグイン用API
+ */
 declare var Plugin: Plugin;
 
 interface IUiComponent<T extends obj, I extends str = str> {
+	/**
+	 * コンポーネントID
+	 */
 	id: I;
-	update: (options: T) => void;
+	/**
+	 * コンポーネントの状態を更新します
+	 * @param options - 各コンポーネントが要求するパラメーター
+	 */
+	update(options: T): void;
 }
 
 interface Component {
+	/**
+	 * コンテナコンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	container(options: {}, id?: str): IUiComponent<typeof options>;
+	/**
+	 * 標準テキストコンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	text(options: {}, id?: str): IUiComponent<typeof options>;
+	/**
+	 * MFMテキストコンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	mfm(options: {}, id?: str): IUiComponent<typeof options>;
-	text(options: {}, id?: str): IUiComponent<typeof options>;
+	/**
+	 * 複数行テキスト入力コンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	textarea(options: {}, id?: str): IUiComponent<typeof options>;
+	/**
+	 * 1行テキスト入力コンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	textInput(options: {}, id?: str): IUiComponent<typeof options>;
+	/**
+	 * 数値入力コンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	numberInput(options: {}, id?: str): IUiComponent<typeof options>;
+	/**
+	 * ボタンコンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	button(options: {}, id?: str): IUiComponent<typeof options>;
+	/**
+	 * 複数ボタンコンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	buttons(options: {}, id?: str): IUiComponent<typeof options>;
+	/**
+	 * プルダウンコンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	switch(options: {}, id?: str): IUiComponent<typeof options>;
+	/**
+	 * フォルダーコンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	folder(options: {}, id?: str): IUiComponent<typeof options>;
+	/**
+	 * 投稿フォーム表示ボタンコンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	postFormButton(options: {}, id?: str): IUiComponent<typeof options>;
+	/**
+	 * 投稿フォーム(そのもの)コンポーネント
+	 * @param options - パラメーター
+	 * @param id - コンポーネントID(必要であれば)
+	 */
 	postForm(options: {}, id?: str): IUiComponent<typeof options>;
 }
 
 declare interface Ui {
+	/**
+	 * rootコンポーネント
+	 */
 	readonly root: IUiComponent<{ children: arr<IUiComponent<obj>> }, '___root___'>;
+	/**
+	 * レンダー関数
+	 * 
+	 * `Ui.root.update({})`の糖衣構文です。
+	 * @param children - コンポーネント
+	 */
 	render(children: arr<IUiComponent<obj>>): void;
+	/**
+	 * コンポーネントIDからコンポーネントを取得します。
+	 * @param id - コンポーネントID
+	 */
 	get(id: str): IUiComponent<obj> | null;
 
+	/**
+	 * Uiコンポーネント
+	 */
 	readonly C: Component;
 }
+/**
+ * Ui API
+ */
 declare var Ui: Ui;
-
